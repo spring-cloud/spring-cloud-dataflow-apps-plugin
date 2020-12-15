302cc45ea828fee8cfcb24abd9477e8a371a9493
Soby Chacko <schacko@pivotal.io>
2020-12-15 13:45:06 -0500

    Build back to snapshots.

--- jobs/springclouddataflowappsmavenplugins/spring_cloud_dataflow_apps_plugins.groovy
+++ jobs/springclouddataflowappsmavenplugins/spring_cloud_dataflow_apps_plugins.groovy
@@ -6,5 +6,5 @@ import javaposse.jobdsl.dsl.DslFactory
 DslFactory dsl = this
 
 // CI
-new SpringCloudDataFlowAppsMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-dataflow-apps-plugin").deploy(false, true)
+new SpringCloudDataFlowAppsMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-dataflow-apps-plugin").deploy()
 
