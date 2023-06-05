ec4c0521722404d8fbb300bad5226b2c5ff85a39
Soby Chacko <schacko@pivotal.io>
2023-06-05 15:52:36 -0400

    Disabling release build.

--- jobs/springclouddataflowappsmavenplugins/spring_cloud_dataflow_apps_plugins.groovy
+++ jobs/springclouddataflowappsmavenplugins/spring_cloud_dataflow_apps_plugins.groovy
@@ -6,4 +6,4 @@ import javaposse.jobdsl.dsl.DslFactory
 DslFactory dsl = this
 
 // CI. Toggle second parameter on the call to deploy with true/false to enable/disable GA release
-new SpringCloudDataFlowAppsMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-dataflow-apps-plugin").deploy(false, true)
+new SpringCloudDataFlowAppsMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-dataflow-apps-plugin").deploy(false, false)
